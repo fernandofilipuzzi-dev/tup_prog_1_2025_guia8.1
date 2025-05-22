5d5ba9fa4372de7d4c5d4f7f11090411dc0b19d2
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2025-05-22 00:54:33 -0300

    ejercicio3

--- Guia8.1/Ejercicio3/Models/Servicio.cs
+++ Guia8.1/Ejercicio3/Models/Servicio.cs
@@ -3,5 +3,61 @@ namespace Ejercicio3.Models
 {
     internal class Servicio
     {
+        public string Nombre0;
+        public int NumeroLibreta0;
+        public string Nombre1;
+        public int NumeroLibreta1;
+        public string Nombre2;
+        public int NumeroLibreta2;
+        public int Orden = 0;
+
+        public void RegistrarNombreYNumeroLibreta(string nombre, int numeroLibreta)
+        {
+            if (Orden == 0)
+            {
+                Nombre0 = nombre;
+                NumeroLibreta0 = numeroLibreta;
+            }
+            else if (Orden == 1)
+            {
+                if (numeroLibreta < NumeroLibreta0)
+                {
+                    Nombre1 = Nombre0;
+                    NumeroLibreta1 = NumeroLibreta0;
+                    Nombre0 = nombre;
+                    NumeroLibreta0 = numeroLibreta;
+                }
+                else
+                {
+                    Nombre1 = nombre;
+                    NumeroLibreta1 = numeroLibreta;
+                }
+            }
+            else if (Orden == 2)
+            {
+                if (numeroLibreta < NumeroLibreta0)
+                {
+                    Nombre2 = Nombre1;
+                    NumeroLibreta2 = NumeroLibreta1;
+                    Nombre1 = Nombre0;
+                    NumeroLibreta1 = NumeroLibreta0;
+                    Nombre0 = nombre;
+                    NumeroLibreta0 = numeroLibreta;
+                }
+                if (numeroLibreta < NumeroLibreta1)
+                {
+                    Nombre2 = Nombre1;
+                    NumeroLibreta2 = NumeroLibreta1;
+                    Nombre1 = nombre;
+                    NumeroLibreta1 = numeroLibreta;
+                }
+                else
+                {
+                    Nombre2 = nombre;
+                    NumeroLibreta2 = numeroLibreta;
+                }
+            }
+            Orden++;
+        }
     }
 }
--- Guia8.1/Ejercicio3/Program.cs
+++ Guia8.1/Ejercicio3/Program.cs
@@ -1,10 +1,84 @@
-﻿
+﻿using Ejercicio3.Models;
+
 namespace Ejercicio3
 {
     internal class Program
     {
+        static Servicio servicio=new Servicio();
+
+        #region metodos 
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese las siguiente opciones:\n\n");
+            Console.WriteLine("1- Registrar las notas de los tres alumnos");
+            Console.WriteLine("2- Mostrar lista ordenada");
+            Console.WriteLine("(otro)- Salir.");
+            int op = Convert.ToInt32(Console.ReadLine());
+            return op;
+        }
+
+        static void MostrarPantallaSolicitarAlumnos()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese los nombres de alumnos y el numero de libreta de cada uno \n\n");
+
+            for (int nro = 0; nro < 3; nro++)
+            {
+                Console.WriteLine("Alumno: " + (nro + 1) + "\n");
+                string nombre = Console.ReadLine();
+                int nota = Convert.ToInt32(Console.ReadLine());
+
+                servicio.RegistrarNombreYNumeroLibreta(nombre, nota);
+            }
+
+            Console.WriteLine("\n\n\nPresione una tecla para continuar");
+            Console.ReadKey();
+        }
+
+        static void MostrarPantallaMostrarListaOrdenada()
+        {
+            Console.Clear();
+            Console.WriteLine("Reparto de dinero: \n\n");
+
+            Console.WriteLine($"{servicio.Nombre0,20}|{servicio.NumeroLibreta0,10}");
+            Console.WriteLine($"{servicio.Nombre1,20}|{servicio.NumeroLibreta1,10}");
+            Console.WriteLine($"{servicio.Nombre2,20}|{servicio.NumeroLibreta2,10}");
+
+            Console.WriteLine("\n\n\nPresione una tecla para volver al menú principal");
+            Console.ReadKey();
+        }
+
+        #endregion
+
         static void Main(string[] args)
         {
+            int op = MostrarPantallaSolicitarOpcionMenu();
+
+            #region iterar opciones menú
+            while (op != -1)
+            {
+                #region verificar opción
+                switch (op)
+                {
+                    case 1:
+                        MostrarPantallaSolicitarAlumnos();
+                        break;
+                    case 2:
+                        MostrarPantallaMostrarListaOrdenada();
+                        break;
+                    default:
+                        op = -1;
+                        break;
+                }
+                #endregion
+
+                #region solicitar opción
+                if (op != -1)
+                    op = MostrarPantallaSolicitarOpcionMenu();
+                #endregion
+            }
+            #endregion
         }
     }
 }
